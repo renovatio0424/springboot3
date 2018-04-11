935f116bd3c16025250c3ee86c7fc4b2b0ac58f2
renov <renovatio0424@gmail.com>
2018-04-11 15:21:45 +0900

    Initial commit

---
+++ src/main/java/com/reno/springboot3/HelloController.java
@@ -0,0 +1,28 @@
+package com.reno.springboot3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public ModelAndView index(ModelAndView modelAndView){
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+    @RequestMapping("/other")
+    public String other(){
+        return "redirect:/";
+    }
+
+    @RequestMapping("/home")
+    public String home(){
+        return "forward:/";
+    }
+}
